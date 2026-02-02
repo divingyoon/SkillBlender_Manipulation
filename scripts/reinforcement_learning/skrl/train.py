9767188907ebef0108d695ab53c16c2ca0b0931f
divingyoon <ukwh5681@gmail.com>
2026-02-02 15:18:03 +0900

    skrl 학습 추가2

--- scripts/reinforcement_learning/skrl/train.py
+++ scripts/reinforcement_learning/skrl/train.py
@@ -178,8 +178,10 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     env_cfg.seed = agent_cfg["seed"]
 
     # specify directory for logging experiments
-    sb_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
-    log_root_path = os.path.join(sb_root, "logs", "skrl", agent_cfg["agent"]["experiment"]["directory"])
+    task_name = args_cli.task.split("-")[0]
+    sbm_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+    log_root_path = os.path.join(sbm_root, "log", "skrl", task_name)
+    os.makedirs(log_root_path, exist_ok=True)
     print(f"[INFO] Logging experiment in directory: {log_root_path}")
     # specify directory for logging runs: {time-stamp}_{run_name}
     log_dir = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + f"_{algorithm}_{args_cli.ml_framework}"
